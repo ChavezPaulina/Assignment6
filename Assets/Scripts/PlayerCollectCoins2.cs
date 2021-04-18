00ddfc390fd267a4bb88040975601bf1c021cc82
ChavezPaulina <77360224+ChavezPaulina@users.noreply.github.com>
2021-04-17 23:30:17 -0500

    Assignment6

---
+++ Assets/Scripts/PlayerCollectCoins2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerCollectCoins2 : MonoBehaviour
+{
+
+    public int points = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(40, 40, 240, 80), "Score:  " + points);
+    }
+
+
+
+}
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+ 
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+}
